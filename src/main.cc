3fb716602a38e06c834e29363b6f31bf9bb1321d
liangjun.cao <yq_caoforever@163.com>
2021-04-20 00:32:38 +0800

    add version info

--- src/main.cc
+++ src/main.cc
@@ -3,11 +3,18 @@
 #include "CLI/Formatter.hpp"
 #include "CLI/Config.hpp"
 
+#define version_info  "  jcc version 0.0.1\n\
+  a toy compiler\n\
+  COPYRIGHT@JUN\n"
+
 int main(int argc, char** argv){
-  CLI::App app{"jcc description"};
-  bool flag{false};
-  app.add_flag("-v", flag, "Flag description.");
+  CLI::App app{"jcc the noob's compiler"};
+  bool version{false};
+  app.add_flag("-v", version, "Flag description.");
   CLI11_PARSE(app, argc, argv);
-  std::cout << "hello jcc" << std::endl;
+
+  if(version){
+    std::cout << version_info;
+  }
   return 0;
 }
