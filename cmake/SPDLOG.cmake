dd7767934e05418b508f15c025dbd606fc6f3c10
liangjun.cao <yq_caoforever@l63.com>
2021-04-22 18:03:47 +0800

    general commit

--- cmake/SPDLOG.cmake
+++ cmake/SPDLOG.cmake
@@ -2,11 +2,13 @@ include(ExternalProject)
 
 set(SPD_SRC_PATH "${CMAKE_BINARY_DIR}/spdlog-src")
 set(SPD_BUILD_PATH "${CMAKE_BINARY_DIR}/spdlog-build")
+set(SPD_BUILD_TYPE ${CMAKE_BUILD_TYPE})
 ExternalProject_Add(SPDLOG
     GIT_REPOSITORY     git@github.com:gabime/spdlog.git
     GIT_TAG            "v1.8.5"
     SOURCE_DIR         ${SPD_SRC_PATH} 
     BINARY_DIR         ${SPD_BUILD_PATH}
+    CMAKE_ARGS         -DCMAKE_BUILD_TYPE=${SPD_BUILD_TYPE}
     CONFIGURE_COMMAND  ${CMAKE_COMMAND} ${SPD_SRC_PATH}
     BUILD_COMMAND      ${CMAKE_COMMAND} --build ${SPD_BUILD_PATH}
     INSTALL_COMMAND    ""
@@ -14,4 +16,8 @@ ExternalProject_Add(SPDLOG
 )
 
 ExternalProject_Get_Property(SPDLOG BINARY_DIR)
-set(SPDLOG_PATH ${BINARY_DIR}/${CMAKE_CFG_INTDIR}/spdlogd.lib)
+if(${SPD_BUILD_TYPE} MATCHES "Debug")
+  set(SPDLOG_PATH ${BINARY_DIR}/${CMAKE_CFG_INTDIR}/spdlogd.lib)
+else()
+  set(SPDLOG_PATH ${BINARY_DIR}/${CMAKE_CFG_INTDIR}/spdlog.lib)
+endif()
--- src/main.cc
+++ src/main.cc
@@ -8,28 +8,27 @@
 #include "CLI/Config.hpp"
 #include "spdlog/spdlog.h"
 
-
 #define version_info  "  jcc version '0.0.1'\n\
   a toy compiler\n\
   COPYRIGHT@JUN"
 
 int main(int argc, char** argv) {
 
+  // parse args 
   CLI::App app{"the noob's compiler jcc"};
-  // version 
   bool version{false};
   app.add_flag("-v, --v", version, "Version description")->ignore_case();
-  // source_code_path
   std::vector<std::string> include_pathes;
   app.add_option("-I", include_pathes, "Source files path");
-  // parse args 
   CLI11_PARSE(app, argc, argv);
   
   if(version) {
     std::cout << version_info << std::endl;
     exit(0);
   }
+
   auto jcc = Jcc::GetJcc(); 
   jcc->SetIncludePathes(include_pathes);
   return 0;
+
 }
