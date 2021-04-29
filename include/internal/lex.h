d3c85cb09bae2213719c1c1fcc97eeb31b742039
liangjun.cao <yq_caoforever@l63.com>
2021-04-29 18:22:27 +0800

    genearl commit

--- include/internal/lex.h
+++ include/internal/lex.h
@@ -1,6 +1,7 @@
 #ifndef TOKEN_H_
 #define TOKEN_H_
 #include <unordered_map> 
+#include <shared_mutex>
 #include <vector>
 #include <variant>
 #include "c_types.h"
@@ -9,29 +10,30 @@ namespace jcc {
 
 using Lexical_Desc = std::variant<int, double, std::string>;
 
+// describe a token name and its id in token_table 
 struct lexical_unit {
   Lexical_Desc desc;
   uint64_t lex_id;
 };
 
+// token_table define
 class token_table {
  public:
   token_table() = default; 
+  void insertToken(lexical_unit& lexical_unit);
   struct item {
     base_type type;
     std::string value;
   };
  private:
+  struct lex_visitor{
+    template <typename T>
+    void operator()(T& ref){
+     items_.size(); 
+    }
+  } visitor;
   std::vector<item> items_;
+  std::shared_mutex shared_lock_;
 };
-
-class grammar {
- public:
-
- private:
-  std::unordered_map<std::string,int> generated_exprs_;
-};
-
-
 }
 #endif
---
+++ src/internal/lex.cc
@@ -0,0 +1,9 @@
+#include "internal/lex.h"
+
+namespace jcc {
+  
+  void token_table::insertToken(lexical_unit& lexical_unit) {
+    
+  }
+
+}
