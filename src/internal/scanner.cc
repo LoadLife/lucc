e9783797ea23ffd897a79fbeba864bf66415438f
jerry.c <yq_caoforever@outlook.com>
2024-04-28 23:22:00 -0700

    update 4/29/2024

--- src/internal/scanner.cc
+++ src/internal/scanner.cc
@@ -8,23 +8,25 @@ std::unique_ptr<Token> Scanner::Scan() {
   cur_token_str_ = "";
   while (ScanTry(' '))
     cur_ws_ += 1;
-  auto token_loc = cur_loc_; 
-  //token_loc.print();
+  auto token_loc = cur_loc_;
+  // token_loc.print();
 
   auto c = ScanNext();
-  if (c == '\n') 
-    return std::make_unique<Token>(NEW_LINE, token_loc, cur_ws_, cur_token_str_);
+  if (c == '\n')
+    return std::make_unique<Token>(NEW_LINE, token_loc, cur_ws_,
+                                   cur_token_str_);
 
   cur_token_str_ += c;
   switch (c) {
   case '+':
     if (ScanTry('+')) {
       cur_token_str_ += '+';
-    } else if (ScanTry('=')){
+    } else if (ScanTry('=')) {
       cur_token_str_ += '=';
     }
     LOG_INFO("token_str: {}", cur_token_str_);
-    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_), token_loc, cur_ws_, cur_token_str_);
+    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_),
+                                   token_loc, cur_ws_, cur_token_str_);
   case '-':
     if (ScanTry('-')) {
       cur_token_str_ += '-';
@@ -34,37 +36,46 @@ std::unique_ptr<Token> Scanner::Scan() {
       cur_token_str_ += '=';
     }
     LOG_INFO("token_str: {}", cur_token_str_);
-    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_), token_loc, cur_ws_, cur_token_str_);
+    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_),
+                                   token_loc, cur_ws_, cur_token_str_);
   case '(':
     LOG_INFO("token_str: {}", cur_token_str_);
-    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_), token_loc, cur_ws_, cur_token_str_);
+    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_),
+                                   token_loc, cur_ws_, cur_token_str_);
   case ')':
     LOG_INFO("token_str: {}", cur_token_str_);
-    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_), token_loc, cur_ws_, cur_token_str_);
+    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_),
+                                   token_loc, cur_ws_, cur_token_str_);
   case '{':
     LOG_INFO("token_str: {}", cur_token_str_);
-    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_), token_loc, cur_ws_, cur_token_str_);
+    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_),
+                                   token_loc, cur_ws_, cur_token_str_);
   case '}':
     LOG_INFO("token_str: {}", cur_token_str_);
-    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_), token_loc, cur_ws_, cur_token_str_);
+    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_),
+                                   token_loc, cur_ws_, cur_token_str_);
   case ';':
     LOG_INFO("token_str: {}", cur_token_str_);
-    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_), token_loc, cur_ws_, cur_token_str_);
+    return std::make_unique<Token>(str_keyword_map.at(cur_token_str_),
+                                   token_loc, cur_ws_, cur_token_str_);
   case 'a' ... 'z':
   case 'A' ... 'Z':
-  case '_': case '$':
+  case '_':
+  case '$':
     ParseIdentifier();
-    return std::make_unique<Token>(IDENTIFIER, token_loc, cur_ws_, cur_token_str_);
-  case '0'...'9':
+    return std::make_unique<Token>(IDENTIFIER, token_loc, cur_ws_,
+                                   cur_token_str_);
+  case '0' ... '9':
     ParseNum();
-    return std::make_unique<Token>(IDENTIFIER, token_loc, cur_ws_, cur_token_str_);
+    return std::make_unique<Token>(IDENTIFIER, token_loc, cur_ws_,
+                                   cur_token_str_);
   case '=':
     LOG_INFO("token_str: {}", cur_token_str_);
-    return std::make_unique<Token>(EQUAL, token_loc, cur_ws_, cur_token_str_);  
+    return std::make_unique<Token>(EQUAL, token_loc, cur_ws_, cur_token_str_);
   case '\0':
     return std::make_unique<Token>(END, token_loc, cur_ws_, cur_token_str_);
     LOG_INFO("token_str: {}", "file end");
-    
+
   default:
     return std::make_unique<Token>();
   }
@@ -94,7 +105,7 @@ unsigned char Scanner::ScanNext() {
   return ret;
 }
 
-bool Scanner::ScanTry(const unsigned char& c) {
+bool Scanner::ScanTry(const unsigned char &c) {
   if (ScanPeek() == c) {
     ScanNext();
     return true;
@@ -102,19 +113,18 @@ bool Scanner::ScanTry(const unsigned char& c) {
   return false;
 }
 
-bool Scanner::ScanTest(const unsigned char& c) {
-  if (ScanPeek() == c) 
+bool Scanner::ScanTest(const unsigned char &c) {
+  if (ScanPeek() == c)
     return true;
   return false;
 }
 
 void Scanner::ParseIdentifier() {
   auto c = ScanPeek();
-  while (std::isalnum(c) || c == '_' || c == '$')
-  {
+  while (std::isalnum(c) || c == '_' || c == '$') {
     cur_token_str_ += c;
     ScanNext();
-    c = ScanPeek(); 
+    c = ScanPeek();
   }
   LOG_INFO("token_str: {}", cur_token_str_);
 }
@@ -129,4 +139,4 @@ void Scanner::ParseNum() {
   LOG_INFO("token_str: {}", cur_token_str_);
 }
 
-} //namespace lucc
+} // namespace lucc
